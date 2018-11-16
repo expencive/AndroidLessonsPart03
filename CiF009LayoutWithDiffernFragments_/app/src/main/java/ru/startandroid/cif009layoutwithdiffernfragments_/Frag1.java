05319c179037194f8b480c0c8594478030874a80
expencive <43270198+expencive@users.noreply.github.com>
2018-11-16 16:57:09 +0300

    LayoutWithDifferentFragments

---
+++ CiF009LayoutWithDiffernFragments_/app/src/main/java/ru/startandroid/cif009layoutwithdiffernfragments_/Frag1.java
@@ -0,0 +1,21 @@
+package ru.startandroid.cif009layoutwithdiffernfragments_;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Настик on 16.11.2018.
+ */
+
+public class Frag1 extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.frag1_layout, container, false);
+    }
+}
