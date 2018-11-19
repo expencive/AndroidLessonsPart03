d733bdff832d43fa5b94f5c1cc96d2d4ec209ff1
expencive <43270198+expencive@users.noreply.github.com>
2018-11-19 14:52:33 +0300

    RecyclerView

--- CiF013RecyclerViewProject/app/src/main/java/ru/startandroid/cif013recyclerviewproject/MainActivity.java
+++ CiF013RecyclerViewProject/app/src/main/java/ru/startandroid/cif013recyclerviewproject/MainActivity.java
@@ -2,10 +2,15 @@ package ru.startandroid.cif013recyclerviewproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView mRecycleView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +21,13 @@ public class MainActivity extends AppCompatActivity {
         exampleList.add(new ExampleItem(R.drawable.ic_android, "line 1", "line 2"));
         exampleList.add(new ExampleItem(R.drawable.ic_arrow, "line A", "line B"));
         exampleList.add(new ExampleItem(R.drawable.ic_beach_access, "line I", "line II"));
+
+        mRecycleView = (RecyclerView) findViewById(R.id.recyclerView);
+        mRecycleView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(this);
+        mAdapter = new ExampleAdapter(exampleList);
+
+        mRecycleView.setLayoutManager(mLayoutManager);
+        mRecycleView.setAdapter(mAdapter);
     }
 }
