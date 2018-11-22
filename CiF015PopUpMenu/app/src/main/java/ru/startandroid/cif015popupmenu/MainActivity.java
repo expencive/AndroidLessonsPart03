465075c2eacfca642dda4f10780411a7edb24c78
expencive <43270198+expencive@users.noreply.github.com>
2018-11-22 12:21:49 +0300

    PopUpMenu

---
+++ CiF015PopUpMenu/app/src/main/java/ru/startandroid/cif015popupmenu/MainActivity.java
@@ -0,0 +1,49 @@
+package ru.startandroid.cif015popupmenu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.PopupMenu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void showPopUp(View v) {
+
+        PopupMenu popUp = new PopupMenu(this, v);
+
+        popUp.setOnMenuItemClickListener(this);
+
+        popUp.inflate(R.menu.menu);
+        popUp.show();
+
+    }
+
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.item1:
+                Toast.makeText(this, "Item 1 clicked", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item2:
+                Toast.makeText(this, "Item 2 clicked", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item3:
+                Toast.makeText(this, "Item 3 clicked", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item4:
+                Toast.makeText(this, "Item 4 clicked", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return false;
+        }
+    }
+}
