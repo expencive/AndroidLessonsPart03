0f4b5869054fe2a93b2dc7e62c4e12749250ef37
expencive <43270198+expencive@users.noreply.github.com>
2018-11-22 11:03:26 +0300

    DatePickerDialog

---
+++ CiF014DatePickerDialog/app/src/main/java/ru/startandroid/cif014datepickerdialog/MainActivity.java
@@ -0,0 +1,47 @@
+package ru.startandroid.cif014datepickerdialog;
+
+import android.app.DatePickerDialog;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnOpenPicker = (Button) findViewById(R.id.btnOpenPicker);
+        btnOpenPicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment datePicker = new DatePickerFragment();
+                datePicker.show(getSupportFragmentManager(), "date picker");
+            }
+        });
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+
+        Calendar c = Calendar.getInstance();
+        c.set(Calendar.YEAR, year);
+        c.set(Calendar.MONTH, month);
+        c.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+
+        String currentDateString = DateFormat.getDateInstance().format(c.getTime());
+
+        TextView tvDateInsert = (TextView) findViewById(R.id.tvDateInsert);
+
+        tvDateInsert.setText(currentDateString);
+
+    }
+}
