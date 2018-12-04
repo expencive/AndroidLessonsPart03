3f0cf3b46281f395b7ed311d4ad5756d4338ea56
expencive <43270198+expencive@users.noreply.github.com>
2018-12-04 13:07:45 +0300

    EasySplashScreen

---
+++ CiF026EasySplashScreen/app/src/main/java/comexpencive/vk/cif026easysplashscreen/SplashScreenActivity.java
@@ -0,0 +1,37 @@
+package comexpencive.vk.cif026easysplashscreen;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import gr.net.maroulis.library.EasySplashScreen;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        EasySplashScreen config = new EasySplashScreen(SplashScreenActivity.this)
+                .withFullScreen()
+                .withTargetActivity(MainActivity.class)
+                .withSplashTimeOut(2000)
+                .withBackgroundColor(Color.parseColor("#FF4081"))
+                .withHeaderText("Заголовок")
+                .withFooterText("Нижний текст")
+                .withBeforeLogoText("Перед")
+                .withAfterLogoText("После")
+                .withLogo(R.mipmap.ic_launcher_round);
+
+        config.getHeaderTextView().setTextColor(Color.WHITE);
+        config.getFooterTextView().setTextColor(Color.WHITE);
+        config.getBeforeLogoTextView().setTextColor(Color.WHITE);
+        config.getAfterLogoTextView().setTextColor(Color.WHITE);
+
+        View easySplashscreen = config.create();
+        setContentView(easySplashscreen);
+
+
+    }
+}
