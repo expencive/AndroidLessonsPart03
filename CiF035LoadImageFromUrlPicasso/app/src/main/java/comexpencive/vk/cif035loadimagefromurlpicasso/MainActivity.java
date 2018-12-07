50e881a83b62d8de18756e354f44ee239e7ed1a6
expencive <43270198+expencive@users.noreply.github.com>
2018-12-07 22:11:43 +0300

    LoadImages

---
+++ CiF035LoadImageFromUrlPicasso/app/src/main/java/comexpencive/vk/cif035loadimagefromurlpicasso/MainActivity.java
@@ -0,0 +1,23 @@
+package comexpencive.vk.cif035loadimagefromurlpicasso;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView imageView = (ImageView) findViewById(R.id.image_view);
+
+        String url = "https://cdn.pixabay.com/photo/2017/11/06/18/39/apple-2924531_960_720.jpg";
+
+        Picasso.with(this).load(url).into(imageView);
+
+    }
+}
