e54e3984c9233a7264868e0cc5c725706312fb27
expencive <43270198+expencive@users.noreply.github.com>
2018-12-13 08:42:21 +0300

    AnimationChanginActivity

---
+++ CiF049AnimationChanginActivities/app/src/main/java/comexpencive/vk/cif049animationchanginactivities/Main2Activity.java
@@ -0,0 +1,26 @@
+package comexpencive.vk.cif049animationchanginactivities;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+
+    public void openActivity3(View view) {
+        Intent intent = new Intent(this, Main3Activity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public void finish() {
+        super.finish();
+        overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
+    }
+}
---
+++ CiF049AnimationChanginActivities/app/src/main/java/comexpencive/vk/cif049animationchanginactivities/Main3Activity.java
@@ -0,0 +1,13 @@
+package comexpencive.vk.cif049animationchanginactivities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+    }
+}
