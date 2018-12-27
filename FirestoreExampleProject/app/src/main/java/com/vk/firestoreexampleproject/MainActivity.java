957e264a89580caaf1b6fcd2ad29d7bad936a2f1
expencive <43270198+expencive@users.noreply.github.com>
2018-12-27 14:28:44 +0300

    FirestoreExample

--- FirestoreExampleProject/app/src/main/java/com/vk/firestoreexampleproject/MainActivity.java
+++ FirestoreExampleProject/app/src/main/java/com/vk/firestoreexampleproject/MainActivity.java
@@ -6,10 +6,13 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
@@ -23,7 +26,12 @@ public class MainActivity extends AppCompatActivity {
     private EditText editTextTitle;
     private EditText editTextDescription;
 
+    private TextView textViewData;
+
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    private DocumentReference noteRef = db.collection("Notebook").document("MyFirstNote");
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
 
         editTextTitle = findViewById(R.id.edit_text_title);
         editTextDescription = findViewById(R.id.edit_text_description);
+        textViewData = findViewById(R.id.text_view_data);
     }
 
     public void saveNote(View v) {
@@ -41,8 +50,7 @@ public class MainActivity extends AppCompatActivity {
         Map<String, Object> note = new HashMap<>();
         note.put(KEY_TITLE, title);
         note.put(KEY_DESCRIPTION, description);
-
-        db.collection("Notebook").document().set(note)
+        noteRef.set(note)
         .addOnSuccessListener(new OnSuccessListener<Void>() {
             @Override
             public void onSuccess(Void aVoid) {
@@ -56,5 +64,35 @@ public class MainActivity extends AppCompatActivity {
                 Log.d(TAG, e.toString());
             }
         });
+    }
+
+    public void loadNote (View v) {
+        noteRef.get()
+                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+                        if (documentSnapshot.exists()) {
+                            String title = documentSnapshot.getString(KEY_TITLE);
+                            String description = documentSnapshot.getString(KEY_DESCRIPTION);
+
+                            //Map<String, Object> note = documentSnapshot.getData(); // еще один путь получения данных
+
+                            textViewData.setText("Название: " + title + "\n" + "Описание: " + description);
+
+                        } else{
+                            Toast.makeText(MainActivity.this, "Документ не существует", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(MainActivity.this, "Ошибка загрузки файла", Toast.LENGTH_SHORT).show();
+
+                Log.d(TAG, e.toString());
+
+            }
+        });
+
     }
 }
