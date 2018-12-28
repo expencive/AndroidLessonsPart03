f33e5f7a52ee2057338b3aa2df415d189208bed1
expencive <43270198+expencive@users.noreply.github.com>
2018-12-28 11:02:09 +0300

    FireStore

--- FirestoreExampleProject/app/src/main/java/com/vk/firestoreexampleproject/MainActivity.java
+++ FirestoreExampleProject/app/src/main/java/com/vk/firestoreexampleproject/MainActivity.java
@@ -13,10 +13,15 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.ListenerRegistration;
 
 import java.util.HashMap;
 import java.util.Map;
+
+import javax.annotation.Nullable;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
     private DocumentReference noteRef = db.collection("Notebook").document("MyFirstNote");
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,6 +48,34 @@ public class MainActivity extends AppCompatActivity {
         editTextDescription = findViewById(R.id.edit_text_description);
         textViewData = findViewById(R.id.text_view_data);
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        noteRef.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
+                if (e !=null) {
+                    Toast.makeText(MainActivity.this, "Ошибка загрузки", Toast.LENGTH_SHORT).show();
+                    Log.d(TAG, e.toString());
+                    return;
+                }
+                
+                if (documentSnapshot.exists()) {
+                    String title = documentSnapshot.getString(KEY_TITLE);
+                    String description = documentSnapshot.getString(KEY_DESCRIPTION);
+
+                    
+                    textViewData.setText("Название: " + title + "\n" + "Описание: " + description);
+
+                }
+
+            }
+        });
+    }
+
+
 
     public void saveNote(View v) {
         String title = editTextTitle.getText().toString();
