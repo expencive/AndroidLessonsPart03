4a005dfc5b01fda98d0471b209e540a268561281
expencive <43270198+expencive@users.noreply.github.com>
2018-12-28 11:31:22 +0300

    Firestore Example

--- FirestoreExampleProject/app/src/main/java/com/vk/firestoreexampleproject/MainActivity.java
+++ FirestoreExampleProject/app/src/main/java/com/vk/firestoreexampleproject/MainActivity.java
@@ -17,6 +17,7 @@ import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.ListenerRegistration;
+import com.google.firebase.firestore.SetOptions;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -100,6 +101,18 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    public void updateDescription(View v) {
+
+        String description = editTextDescription.getText().toString();
+
+       // Map<String, Object> note = new HashMap<>();
+        //note.put(KEY_DESCRIPTION, description);
+
+        //noteRef.set(note, SetOptions.merge()); // перезаписывает документ полностью и название в том числе
+
+        noteRef.update(KEY_DESCRIPTION, description);
+    }
+
     public void loadNote (View v) {
         noteRef.get()
                 .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
@@ -129,4 +142,6 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+
+
 }
