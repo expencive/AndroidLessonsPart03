88270f926d356180f86ab34290222bed84cdd81f
expencive <43270198+expencive@users.noreply.github.com>
2019-02-13 12:40:20 +0300

    RetrofitExample

---
+++ CiF064RetrofitExample/app/src/main/java/com/gmail/expencive/cif064retrofitexample/JsonPlaceholderApi.java
@@ -0,0 +1,12 @@
+package com.gmail.expencive.cif064retrofitexample;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface JsonPlaceholderApi {
+
+    @GET("posts")
+    Call<List<Post>> getPosts();
+}
