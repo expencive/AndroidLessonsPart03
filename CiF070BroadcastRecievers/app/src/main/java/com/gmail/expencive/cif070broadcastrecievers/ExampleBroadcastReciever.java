9a421ea95b2325c688bf1e2ae2c56059208ae179
expencive <43270198+expencive@users.noreply.github.com>
2019-02-26 11:35:02 +0300

    BroadcastReciever

---
+++ CiF070BroadcastRecievers/app/src/main/java/com/gmail/expencive/cif070broadcastrecievers/ExampleBroadcastReciever.java
@@ -0,0 +1,24 @@
+package com.gmail.expencive.cif070broadcastrecievers;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.widget.Toast;
+
+public class ExampleBroadcastReciever extends BroadcastReceiver {
+
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
+            Toast.makeText(context, "Boot Completed", Toast.LENGTH_SHORT).show();
+
+        }
+
+        if (ConnectivityManager.CONNECTIVITY_ACTION.equals(intent.getAction())) {
+            Toast.makeText(context, "Connectivity changed", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+}
