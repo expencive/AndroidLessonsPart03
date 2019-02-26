99ebd3fa2f2864fe4f1d858a2b0aefa1d2ebf714
expencive <43270198+expencive@users.noreply.github.com>
2019-02-26 12:06:00 +0300

    BroadcastReceiver

---
+++ CiF071BroadcastRecievers/app/src/main/java/com/gmail/expencive/cif071broadcastrecievers/MainActivity.java
@@ -0,0 +1,37 @@
+package com.gmail.expencive.cif071broadcastrecievers;
+
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    ExampleBroadcastReceiver exampleBroadcastReceiver = new ExampleBroadcastReceiver();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+   @Override
+    protected void onStart() {
+        super.onStart();
+
+        IntentFilter filter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
+        registerReceiver(exampleBroadcastReceiver, filter);
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unregisterReceiver(exampleBroadcastReceiver);
+    }
+
+
+
+}
