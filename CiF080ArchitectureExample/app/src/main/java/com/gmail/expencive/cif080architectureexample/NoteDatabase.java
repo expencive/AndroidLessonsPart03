402f71380be09d0390d69ac92329f245bbb977a5
expencive <43270198+expencive@users.noreply.github.com>
2019-03-04 14:57:05 +0300

    ArchitectureExample

--- CiF080ArchitectureExample/app/src/main/java/com/gmail/expencive/cif080architectureexample/NoteDatabase.java
+++ CiF080ArchitectureExample/app/src/main/java/com/gmail/expencive/cif080architectureexample/NoteDatabase.java
@@ -1,10 +1,13 @@
 package com.gmail.expencive.cif080architectureexample;
 
 import android.content.Context;
+import android.os.AsyncTask;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 @Database(entities =  {Note.class}, version = 1)
 public abstract class NoteDatabase extends RoomDatabase {
@@ -20,10 +23,37 @@ public abstract class NoteDatabase extends RoomDatabase {
             instance = Room.databaseBuilder(context.getApplicationContext(),
                     NoteDatabase.class, "note_database")
                     .fallbackToDestructiveMigration()
+                    .addCallback(roomCallback)
                     .build();
         }
 
         return instance;
 
+    }
+
+    private static RoomDatabase.Callback roomCallback = new RoomDatabase.Callback() {
+
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+            new PopulateDbAsyncTasc(instance).execute();
+        }
+    };
+
+    private static class PopulateDbAsyncTasc extends AsyncTask<Void, Void, Void> {
+
+        private NoteDao noteDao;
+
+        private PopulateDbAsyncTasc(NoteDatabase db) {
+            noteDao = db.noteDao();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            noteDao.insert(new Note("Название 1", "Описание 1", 1));
+            noteDao.insert(new Note("Название 2", "Описание 2", 2));
+            noteDao.insert(new Note("Название 3", "Описание 3", 3));
+            return null;
+        }
     }
 }
