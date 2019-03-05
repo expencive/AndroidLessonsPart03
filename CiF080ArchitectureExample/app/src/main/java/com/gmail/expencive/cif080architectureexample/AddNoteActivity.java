509a7c84ae7421a91cb00f4a070ddbc056e71019
expencive <43270198+expencive@users.noreply.github.com>
2019-03-05 15:15:49 +0300

    ArchitectureExample

--- CiF080ArchitectureExample/app/src/main/java/com/gmail/expencive/cif080architectureexample/AddNoteActivity.java
+++ CiF080ArchitectureExample/app/src/main/java/com/gmail/expencive/cif080architectureexample/AddNoteActivity.java
@@ -2,11 +2,25 @@ package com.gmail.expencive.cif080architectureexample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.NumberPicker;
+import android.widget.Toast;
 
 public class AddNoteActivity extends AppCompatActivity {
+    public static final String EXTRA_TITLE =
+            "com.gmail.expencive.cif080architectureexample.EXTRA_TITLE";
+
+    public static final String EXTRA_DESCRIPTION =
+            "com.gmail.expencive.cif080architectureexample.EXTRA_DESCRIPTION";
+
+    public static final String EXTRA_PRIORITY =
+            "com.gmail.expencive.cif080architectureexample.EXTRA_PRIORITY";
+
     private EditText editTextTitle, editTextDescription;
     private NumberPicker numberPickerPriority;
 
@@ -24,5 +38,44 @@ public class AddNoteActivity extends AppCompatActivity {
 
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_close);
         setTitle("Add Note");
+    }
+
+    private void saveNote(){
+        String title = editTextTitle.getText().toString();
+        String description = editTextDescription.getText().toString();
+        int priority = numberPickerPriority.getValue();
+
+        if (title.trim().isEmpty() || description.trim().isEmpty()) {
+            Toast.makeText(this, "Please insert a title and description", Toast.LENGTH_SHORT).show();
+            return;
+
+        }
+
+        Intent data = new Intent();
+        data.putExtra(EXTRA_TITLE, title);
+        data.putExtra(EXTRA_DESCRIPTION, description);
+        data.putExtra(EXTRA_PRIORITY, priority);
+
+        setResult(RESULT_OK, data);
+        finish();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.add_note_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.save_note:
+                saveNote();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
     }
 }
