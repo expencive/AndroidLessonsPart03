5e07b4d27331ec324b4233f7b689f3392341fc3a
expencive <expencive@bk.ru>
2019-04-18 13:51:29 +0300

    Fragment

--- FragmentBottomNavigation/app/src/main/java/expencive/vk/com/fragmentbottomnavigation/HomeFragment.java
+++ FragmentBottomNavigation/app/src/main/java/expencive/vk/com/fragmentbottomnavigation/HomeFragment.java
@@ -1,5 +1,6 @@
 package expencive.vk.com.fragmentbottomnavigation;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,12 +8,96 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import expencive.vk.com.fragmentbottomnavigation.api.ApiClient;
+import expencive.vk.com.fragmentbottomnavigation.api.ApiInterface;
+import expencive.vk.com.fragmentbottomnavigation.models.Animal;
+import expencive.vk.com.fragmentbottomnavigation.models.Animals;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class HomeFragment extends Fragment {
+    private TextView textViewResultHome;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+
+        textViewResultHome = rootView.findViewById(R.id.text_view_result_home);
+
+        return rootView;
+
     }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        loadJson();
+
+
+    }
+
+    public void loadJson(){
+
+        ApiInterface apiInterface = ApiClient.getApiClient().create(ApiInterface.class);
+        //String cat = "cat";
+
+        Call<Animals> call;
+
+        call = apiInterface.getCats();
+
+        call.enqueue(new Callback<Animals>() {
+            @Override
+            public void onResponse(Call<Animals> call, Response<Animals> response) {
+                if (response.isSuccessful() && response.body().getAnimalList()!=null) {
+
+                    List<Animal> animals = new ArrayList<>();
+                    animals = response.body().getAnimalList();
+                    String content = "";
+
+
+
+                    for (Animal animal: animals){
+
+
+
+                        content += "ImageUrl: " + animal.getImageUrl() + "\n";
+                        content += "Title: " + animal.getImageTitle() + "\n\n";
+
+
+
+
+                    }
+
+                    textViewResultHome.setText(content);
+
+
+
+                    return;
+                } else{
+                    Toast.makeText(getContext(), "not succesful", Toast.LENGTH_SHORT).show();
+                }
+
+
+            }
+
+            @Override
+            public void onFailure(Call<Animals> call, Throwable t) {
+                textViewResultHome.setText("Error: " + String.valueOf(t.getMessage()));
+
+
+            }
+        });
+
+    }
+
+
 }
--- FragmentBottomNavigation/app/src/main/java/expencive/vk/com/fragmentbottomnavigation/MainActivity.java
+++ FragmentBottomNavigation/app/src/main/java/expencive/vk/com/fragmentbottomnavigation/MainActivity.java
@@ -6,9 +6,27 @@ import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import expencive.vk.com.fragmentbottomnavigation.api.ApiClient;
+import expencive.vk.com.fragmentbottomnavigation.api.ApiInterface;
+import expencive.vk.com.fragmentbottomnavigation.api.RetrofitRequest;
+import expencive.vk.com.fragmentbottomnavigation.models.Animal;
+import expencive.vk.com.fragmentbottomnavigation.models.Animals;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView textView;
+    private RetrofitRequest retrofitRequest;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,8 +35,15 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
         bottomNav.setOnNavigationItemSelectedListener(navListener);
 
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                new HomeFragment()).commit();
+        //getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+               // new HomeFragment()).commit();
+
+        textView = findViewById(R.id.tv_ma);
+//        retrofitRequest = new RetrofitRequest();
+
+        loadJson();
+
+
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -46,4 +71,69 @@ public class MainActivity extends AppCompatActivity {
         }
 
     };
+
+    public void loadJson(){
+
+        ApiInterface apiInterface = ApiClient.getApiClient().create(ApiInterface.class);
+        //String cat = "cat";
+
+        Call<Animals> call;
+
+        call = apiInterface.getCats();
+
+        call.enqueue(new Callback<Animals>() {
+            @Override
+            public void onResponse(Call<Animals> call, Response<Animals> response) {
+                if (response.isSuccessful() && response.body().getAnimalList()!=null) {
+
+                    List<Animal> animals = new ArrayList<>();
+                    animals = response.body().getAnimalList();
+                    String content = "";
+
+
+
+                    for (Animal animal: animals){
+
+
+
+                        content += "ImageUrl: " + animal.getImageUrl() + "\n";
+                        content += "Title: " + animal.getImageTitle() + "\n\n";
+
+
+
+
+                    }
+
+                    textView.setText(content);
+
+
+
+                    return;
+                } else{
+                    Toast.makeText(MainActivity.this, "not succesful", Toast.LENGTH_SHORT).show();
+                }
+
+
+            }
+
+            @Override
+            public void onFailure(Call<Animals> call, Throwable t) {
+                textView.setText("Error: " + String.valueOf(t.getMessage()));
+
+
+            }
+        });
+
+    }
+
+//    public void onClickStartRequest(View view){
+//
+//
+//        String content;
+//           content= retrofitRequest.getmContent();
+//           textView.setText(content);
+//
+//
+//    }
+
 }
