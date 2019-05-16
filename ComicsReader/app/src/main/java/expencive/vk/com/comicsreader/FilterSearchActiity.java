4c30325855b154bf11e17d49ad1687b7a4d2adf7
expencive <expencive@bk.ru>
2019-05-16 13:54:31 +0300

    ComicReader

--- ComicsReader/app/src/main/java/expencive/vk/com/comicsreader/FilterSearchActiity.java
+++ ComicsReader/app/src/main/java/expencive/vk/com/comicsreader/FilterSearchActiity.java
@@ -1,6 +1,8 @@
 package expencive.vk.com.comicsreader;
 
+import android.content.DialogInterface;
 import android.support.annotation.NonNull;
+import android.support.design.chip.Chip;
 import android.support.design.chip.ChipGroup;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AlertDialog;
@@ -14,8 +16,16 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+import expencive.vk.com.comicsreader.Adapter.MyComicAdapter;
 import expencive.vk.com.comicsreader.Common.Common;
+import expencive.vk.com.comicsreader.Model.Comic;
 
 public class FilterSearchActiity extends AppCompatActivity {
 
@@ -54,19 +64,25 @@ public class FilterSearchActiity extends AppCompatActivity {
     }
 
     private void showSearchDialog() {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(FilterSearchActiity.this);
+        alertDialog.setTitle("Search");
+
+        final LayoutInflater inflater = this.getLayoutInflater();
+
+        View filter_layout = inflater.inflate(R.layout.dialog_options, null);
     }
 
     private void showFiltersDialog() {
         AlertDialog.Builder alertDialog = new AlertDialog.Builder(FilterSearchActiity.this);
         alertDialog.setTitle("Select Category");
 
-        LayoutInflater inflater = this.getLayoutInflater();
+        final LayoutInflater inflater = this.getLayoutInflater();
 
         View filter_layout = inflater.inflate(R.layout.dialog_options, null);
 
         final AutoCompleteTextView txt_category = filter_layout.findViewById(R.id.txt_category);
 
-        ChipGroup chipGroup = filter_layout.findViewById(R.id.chip_group);
+        final ChipGroup chipGroup = filter_layout.findViewById(R.id.chip_group);
 
         //create autocomplete
 
@@ -81,7 +97,68 @@ public class FilterSearchActiity extends AppCompatActivity {
                 txt_category.setText("");
 
                 //create tags
+
+                Chip chip = (Chip) inflater.inflate(R.layout.chip_item, null, false);
+                chip.setText(((TextView) view).getText());
+                chip.setOnCloseIconClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        chipGroup.removeView(v);
+                    }
+                });
+
+                chipGroup.addView(chip);
             }
         });
+
+        alertDialog.setView(filter_layout);
+        alertDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        alertDialog.setPositiveButton("Filter", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                List<String> filter_key = new ArrayList<>();
+                StringBuilder filter_query = new StringBuilder("");
+
+                for (int j = 0; j<chipGroup.getChildCount(); j++){
+                    Chip chip = (Chip) chipGroup.getChildAt(j);
+                    filter_key.add(chip.getText().toString());
+
+                    Collections.sort(filter_key);
+
+                    //convert list to string
+
+                    for (String key: filter_key){
+                        filter_query.append(key).append(",");
+                    }
+
+                    //remove last ,
+
+                    filter_query.setLength(filter_query.length() - 1);
+
+                    //filter by this query
+
+                    fetchFilterCategory(filter_query.toString())
+                }
+
+            }
+        });
+        alertDialog.show();
+    }
+
+    private void fetchFilterCategory(String query) {
+        List<Comic> comic_filtered = new ArrayList<>();
+        for (Comic comic: Common.comicList){
+
+            if (comic.Category.contains(query))
+                comic_filtered.add(comic);
+
+        }
+
+        recycler_filter_search.setAdapter(new MyComicAdapter(getBaseContext(), comic_filtered));
     }
 }
